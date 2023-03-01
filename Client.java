158c976432b80a343dc701ccb2ed73b248b325d8
raju-2514 <91734699+raju-2514@users.noreply.github.com>
2023-03-02 03:22:42 +0600

    Using TCP reader-Writer Problem

--- Client.java
+++ Client.java
@@ -1,17 +1,13 @@
-
-package client.server;
+package io.github.hridoy100;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 
 public class Client {
-     public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         System.out.println("Client started..");
         Socket socket = new Socket("127.0.0.1", 22222);
         System.out.println("Client Connected..");
@@ -19,20 +15,9 @@ public class Client {
         ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
         ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
 
-        Scanner sc = new Scanner(System.in);
-
-        String message = sc.nextLine();
-        //sent to server...
-        oos.writeObject(message);
-
-        try {
-            //receive from server..
-            Object fromServer = ois.readObject();
-            System.out.println("From Server: "+(String)fromServer);
-
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        new WriterThread(oos, "Client1");
+        new ReaderThread(ois, "Client1");
+//        socket.close();
 
     }
 }
